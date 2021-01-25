2d7410e8909b3f8976ff474f78d2164104a0a66b
GoelVaibhav009 <creativeprogrammer623@gmail.com>
2021-01-26 02:04:01 +0530

    UserScheme

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'))
 
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
